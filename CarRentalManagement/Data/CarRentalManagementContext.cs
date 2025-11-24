f6ea50be4fb59dd64bfc7b74349d88db1e3e4af3
MUHAMMAD SYAZAD BIN SARJONO 2401659D <2401659D@student.tp.edu.sg>
2025-11-24 15:31:18 +0800

    Added Identity

--- CarRentalManagement/Data/CarRentalManagementContext.cs
+++ CarRentalManagement/Data/CarRentalManagementContext.cs
@@ -1,35 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using CarRentalManagement.Configurations.Entities;
+using CarRentalManagement.Data;
 using CarRentalManagement.Domain;
-using CarRentalManagement.Configurations.Entities;
-
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 namespace CarRentalManagement.Data
 {
-    public class CarRentalManagementContext : DbContext
+    public class CarRentalManagementContext(DbContextOptions<CarRentalManagementContext> options) :
+    IdentityDbContext<CarRentalManagementUser>(options)
     {
-        public CarRentalManagementContext (DbContextOptions<CarRentalManagementContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<CarRentalManagement.Domain.Make> Make { get; set; } = default!;
-        public DbSet<CarRentalManagement.Domain.Model> Model { get; set; } = default!;
-        public DbSet<CarRentalManagement.Domain.Colour> Colour { get; set; } = default!;
-        public DbSet<CarRentalManagement.Domain.Vehicle> Vehicle { get; set; } = default!;
-        public DbSet<CarRentalManagement.Domain.Booking> Booking { get; set; } = default!;
-        public DbSet<CarRentalManagement.Domain.Customer> Customer { get; set; } = default!;
-
+        public DbSet<Make> Make { get; set; } = default!;
+        public DbSet<Model> Model { get; set; } = default!;
+        public DbSet<Colour> Colour { get; set; } = default!;
+        public DbSet<Vehicle> Vehicle { get; set; } = default!;
+        public DbSet<Booking> Booking { get; set; } = default!;
+        public DbSet<Customer> Customer { get; set; } = default!;
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-
             builder.ApplyConfiguration(new ColourSeed());
-
             builder.ApplyConfiguration(new MakeSeed());
-
             builder.ApplyConfiguration(new ModelSeed());
         }
     }
---
+++ CarRentalManagement/Migrations/20251124072904_AddIdentity.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarRentalManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIdentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Colour",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8408), new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8427) });
+
+            migrationBuilder.UpdateData(
+                table: "Colour",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8428), new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8429) });
+
+            migrationBuilder.UpdateData(
+                table: "Make",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8561), new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8561) });
+
+            migrationBuilder.UpdateData(
+                table: "Make",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8563), new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8563) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8623), new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8623) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8624), new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8625) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8626), new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8626) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8627), new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8627) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Colour",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 26, 12, 386, DateTimeKind.Local).AddTicks(1243), new DateTime(2025, 11, 24, 15, 26, 12, 386, DateTimeKind.Local).AddTicks(1258) });
+
+            migrationBuilder.UpdateData(
+                table: "Colour",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 26, 12, 386, DateTimeKind.Local).AddTicks(1259), new DateTime(2025, 11, 24, 15, 26, 12, 386, DateTimeKind.Local).AddTicks(1260) });
+
+            migrationBuilder.UpdateData(
+                table: "Make",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 26, 12, 386, DateTimeKind.Local).AddTicks(1385), new DateTime(2025, 11, 24, 15, 26, 12, 386, DateTimeKind.Local).AddTicks(1385) });
+
+            migrationBuilder.UpdateData(
+                table: "Make",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 26, 12, 386, DateTimeKind.Local).AddTicks(1386), new DateTime(2025, 11, 24, 15, 26, 12, 386, DateTimeKind.Local).AddTicks(1387) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 26, 12, 386, DateTimeKind.Local).AddTicks(1436), new DateTime(2025, 11, 24, 15, 26, 12, 386, DateTimeKind.Local).AddTicks(1436) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 26, 12, 386, DateTimeKind.Local).AddTicks(1439), new DateTime(2025, 11, 24, 15, 26, 12, 386, DateTimeKind.Local).AddTicks(1439) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 26, 12, 386, DateTimeKind.Local).AddTicks(1440), new DateTime(2025, 11, 24, 15, 26, 12, 386, DateTimeKind.Local).AddTicks(1441) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 26, 12, 386, DateTimeKind.Local).AddTicks(1441), new DateTime(2025, 11, 24, 15, 26, 12, 386, DateTimeKind.Local).AddTicks(1442) });
+        }
+    }
+}
