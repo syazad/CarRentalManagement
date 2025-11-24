9829c8c8afe844b6d1aa784e3673461514fa490f
MUHAMMAD SYAZAD BIN SARJONO 2401659D <2401659D@student.tp.edu.sg>
2025-11-24 15:55:00 +0800

    Added seed role and user

---
+++ CarRentalManagement/Migrations/20251124073841_SeedUserRole.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CarRentalManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedUserRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "ad2bcf0c-20db-474f-8407-5a6b159518ba", null, "Administrator", "ADMINISTRATOR" },
+                    { "bd2bcf0c-20db-474f-8407-5a6b159518bb", null, "User", "USER" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "3781efa7-66dc-47f0-860f-e506d04102e4", 0, "95824504-e109-45d3-9257-e9ba0bf8aaf6", "admin@localhost.com", true, "Admin", "User", false, null, "ADMIN@LOCALHOST.COM", "ADMIN@LOCALHOST.COM", "AQAAAAIAAYagAAAAEIvXrvJtljRONogvnUp6ICuba3Zop6Ce1M0aLKtTNow2a2/UBq68oPeBBpmBtmN2KQ==", null, false, "1855ad93-f2f5-4742-9c1f-76627fe90521", false, "admin@localhost.com" });
+
+            migrationBuilder.UpdateData(
+                table: "Colour",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 38, 40, 883, DateTimeKind.Local).AddTicks(5146), new DateTime(2025, 11, 24, 15, 38, 40, 883, DateTimeKind.Local).AddTicks(6224) });
+
+            migrationBuilder.UpdateData(
+                table: "Colour",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 38, 40, 883, DateTimeKind.Local).AddTicks(6236), new DateTime(2025, 11, 24, 15, 38, 40, 883, DateTimeKind.Local).AddTicks(6236) });
+
+            migrationBuilder.UpdateData(
+                table: "Make",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 38, 40, 883, DateTimeKind.Local).AddTicks(6722), new DateTime(2025, 11, 24, 15, 38, 40, 883, DateTimeKind.Local).AddTicks(6728) });
+
+            migrationBuilder.UpdateData(
+                table: "Make",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 38, 40, 883, DateTimeKind.Local).AddTicks(6732), new DateTime(2025, 11, 24, 15, 38, 40, 883, DateTimeKind.Local).AddTicks(6733) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 38, 40, 883, DateTimeKind.Local).AddTicks(6897), new DateTime(2025, 11, 24, 15, 38, 40, 883, DateTimeKind.Local).AddTicks(6898) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 38, 40, 883, DateTimeKind.Local).AddTicks(6902), new DateTime(2025, 11, 24, 15, 38, 40, 883, DateTimeKind.Local).AddTicks(6903) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 38, 40, 883, DateTimeKind.Local).AddTicks(6906), new DateTime(2025, 11, 24, 15, 38, 40, 883, DateTimeKind.Local).AddTicks(6907) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 38, 40, 883, DateTimeKind.Local).AddTicks(6910), new DateTime(2025, 11, 24, 15, 38, 40, 883, DateTimeKind.Local).AddTicks(6911) });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "ad2bcf0c-20db-474f-8407-5a6b159518ba", "3781efa7-66dc-47f0-860f-e506d04102e4" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "bd2bcf0c-20db-474f-8407-5a6b159518bb");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "ad2bcf0c-20db-474f-8407-5a6b159518ba", "3781efa7-66dc-47f0-860f-e506d04102e4" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ad2bcf0c-20db-474f-8407-5a6b159518ba");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "3781efa7-66dc-47f0-860f-e506d04102e4");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.UpdateData(
+                table: "Colour",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8408), new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8427) });
+
+            migrationBuilder.UpdateData(
+                table: "Colour",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8428), new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8429) });
+
+            migrationBuilder.UpdateData(
+                table: "Make",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8561), new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8561) });
+
+            migrationBuilder.UpdateData(
+                table: "Make",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8563), new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8563) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8623), new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8623) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8624), new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8625) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8626), new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8626) });
+
+            migrationBuilder.UpdateData(
+                table: "Model",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8627), new DateTime(2025, 11, 24, 15, 29, 4, 15, DateTimeKind.Local).AddTicks(8627) });
+        }
+    }
+}
